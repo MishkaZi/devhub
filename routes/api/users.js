1f82aaaaac0a05d8f407dcf1ceb88b913c0377d0
Michael Zinoviev <mishazinoviev91@gmail.com>
2020-09-25 15:05:13 +0300

    Validation addition

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
 //@route  POST api/users
 //@desc   Register user
@@ -12,7 +12,11 @@ router.post(
   check('password', 'Enter a password with 6 or more letters!').isLength({min:6})
     ],
   (req, res) => {
-    console.log(req.body);
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors:errors.array()});
+    }
+
     res.send('User route');
   }
 );
