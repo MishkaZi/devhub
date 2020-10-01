7ac196b4e3d255fa75c55c106070312786e04c88
Michael Zinoviev <mishazinoviev91@gmail.com>
2020-10-01 12:26:46 +0300

    Added alerts with redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,30 @@ import Landing from './components/layout/Landing';
 
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+import Alert from './components/layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-  <Router className='App'>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/Login' component={Login} />
-        </Switch>
-      </section>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router className='App'>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/Login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
 );
 
 export default App;
