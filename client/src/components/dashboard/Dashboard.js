0c93763626b7ed5b45736226b1cd09db31e718e1
Michael Zinoviev <mishazinoviev91@gmail.com>
2020-10-04 18:27:12 +0300

    Added edit profile (There is a bug, needed to be fixed)

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
+import DashboardActions from '../dashboard/DashboardActions';
 import { Link } from 'react-router-dom';
 
 const Dashboard = ({
@@ -22,7 +23,12 @@ const Dashboard = ({
       <p class='lead'>
         <i class='fas fa-user'></i> Welcome {user && user.name}
       </p>
-      {profile !== null ? <Fragment>has</Fragment> : <Fragment>
+      {profile !== null ? 
+       <Fragment>
+          <DashboardActions/>
+       </Fragment>
+       : 
+       <Fragment>
         <p>You have not yet set an profile, add some info!</p>
         <Link to='/create-profile' className='btb btn-primary my-1'>Create Profile</Link>
       </Fragment>}
