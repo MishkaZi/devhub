782e31a9ca42564684e61f868b3f6e5504776ec0
Michael Zinoviev <mishazinoviev91@gmail.com>
2020-10-07 17:34:54 +0300

    Added not found page

--- client/src/components/routing/Routes.js
+++ client/src/components/routing/Routes.js
@@ -11,7 +11,7 @@ import Profiles from '../profiles/Profiles';
 import Profile from '../profile/Profile';
 import Posts from '../posts/Posts';
 import Post from '../post/Post';
-//import NotFound from '../layout/NotFound';
+import NotFound from '../layout/NotFound';
 import PrivateRoute from '../routing/PrivateRoute';
 
 const Routes = (props) => {
@@ -29,8 +29,8 @@ const Routes = (props) => {
         <PrivateRoute exact path='/add-experience' component={AddExperience} />
         <PrivateRoute exact path='/add-education' component={AddEducation} />
         <PrivateRoute exact path='/posts' component={Posts} />
-        <PrivateRoute exact path="/posts/:id" component={Post} />
-        {/* <Route component={NotFound} />  */}
+        <PrivateRoute exact path='/posts/:id' component={Post} />
+        <Route component={NotFound} />
       </Switch>
     </section>
   );
